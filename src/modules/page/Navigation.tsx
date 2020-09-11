052f49920ff1d0ae555d2d7f9c6f55f8955eae3b
Simen A. W. Olsen <cobraz@cobraz.no>
2020-09-11 15:54:13 +0200

    (Navigation) Add initial implementation

--- src/modules/page/Navigation.tsx
+++ src/modules/page/Navigation.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Heading } from 'theme-ui';
 import Layout from '../../components/Layout';
 
-const Navigation = () => <Layout>Navigation</Layout>;
+const Navigation = () => {
+    
+  return (
+    <Layout>
+      <Heading>Hovedmeny</Heading>
+
+      <Heading>Bunntekst</Heading>
+
+      <Heading>Ikke i navigasjon</Heading>
+    </Layout>
+  );
+};
 
 export default Navigation;

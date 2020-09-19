73c685f060b55b77a0897e0cfc83405c3450f953
Simen A. W. Olsen <cobraz@cobraz.no>
2020-09-20 01:19:22 +0200

    feat: Add Shipping Settings
    fix #12

--- src/modules/settings/Settings.tsx
+++ src/modules/settings/Settings.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../../components/Layout';
 
-const Settings = () => <Layout>Settings</Layout>;
+const Settings = () => <Layout><Link to="/settings/shipping">Leveringsmetoder</Link></Layout>;
 
 export default Settings;
--- src/modules/settings/ShippingSettings.tsx
+++ src/modules/settings/ShippingSettings.tsx
@@ -1,6 +1,58 @@
+import { Table, icons } from '@tabetalt/kit';
 import React from 'react';
+import { Box, Heading, IconButton, Text } from 'theme-ui';
 import Layout from '../../components/Layout';
 
-const Shipping = () => <Layout>Shipping</Layout>;
+const Shipping = () => {
+  const data = Array(4).fill({
+    name: 'PostNord',
+    price: '299,90 NOK',
+    status: 'Aktiv',
+    actions: (
+      <Box sx={{ textAlign: 'right' }}>
+        <IconButton>
+          <icons.TrashIcon />
+        </IconButton>
+        <IconButton>
+          <icons.PencilIcon />
+        </IconButton>
+      </Box>
+    ),
+  });
+  return (
+    <Layout>
+      <Box sx={{ p: 5 }}>
+        <Heading>Leveringsmetoder</Heading>
+        <Text sx={{mb: 4}}>
+          Hvilke leveringsmetoder vil du tilby? Her kan du administrere,
+          sortere, legge til eller slette leveringsmetoder som kundene dine kan
+          bruke.
+        </Text>
+        <Table
+          columns={[
+            {
+              Header: 'Leveringsmetode',
+              accessor: 'name',
+              minWidth: '50%',
+            },
+            {
+              Header: 'Pris',
+              accessor: 'price',
+            },
+            {
+              Header: 'Status',
+              accessor: 'status',
+            },
+            {
+              Header: '',
+              accessor: 'actions',
+            },
+          ]}
+          data={data}
+        />
+      </Box>
+    </Layout>
+  );
+};
 
 export default Shipping;

a8e6e8bd1b86d29ff08ef22178ca3b7f7035e1fa
Simen A. W. Olsen <cobraz@cobraz.no>
2020-09-20 15:40:40 +0200

    feat: Add ProductVariants
    fix #18

--- src/modules/catalog/product/components/ProductVariants.tsx
+++ src/modules/catalog/product/components/ProductVariants.tsx
@@ -1,8 +1,44 @@
+import { Field, Table } from '@tabetalt/kit';
 import React from 'react';
+import { Box, Heading } from 'theme-ui';
 import { ProductAttr } from '../Product';
 
-const ProductVariants: React.FC<{ product: ProductAttr }> = () => (
-  <div>Here comes product variants</div>
+const ProductVariants: React.FC<{ product: ProductAttr }> = ({ product }) => (
+  <Box>
+    <Box sx={{ maxWidth: 820, '> div': { mb: 3 } }}>
+      <Heading as="h4">Variantgrupper</Heading>
+      <Field label="Størrelser" name="size" value="XL, Small, XS" />
+      <Field label="Farger" name="color" value="Lilla, Burgunder rød" />
+    </Box>
+    <Box sx={{ mt: 4 }}>
+      <Heading as="h4">Variantinnstillinger</Heading>
+      <Table
+        columns={[
+          {
+            Header: 'Størrelse',
+            accessor: 'size',
+          },
+          {
+            Header: 'Farge',
+            accessor: 'color',
+          },
+          {
+            Header: 'Pris inkl. MVA',
+            accessor: 'price',
+          },
+          {
+            Header: 'Lagerbeholdning',
+            accessor: 'stock',
+          },
+          {
+            Header: 'Tilgjengelig',
+            accessor: 'available',
+          },
+        ]}
+        data={product.variants}
+      />
+    </Box>
+  </Box>
 );
 
 export default ProductVariants;

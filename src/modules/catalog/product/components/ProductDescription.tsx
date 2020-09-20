b152491a2869b596388d8d0fed7f6c1db190224a
Simen A. W. Olsen <cobraz@cobraz.no>
2020-09-20 15:28:08 +0200

    feat: Add Product Description
    fix #17

--- src/modules/catalog/product/components/ProductDescription.tsx
+++ src/modules/catalog/product/components/ProductDescription.tsx
@@ -1,8 +1,28 @@
+import { Field } from '@tabetalt/kit';
 import React from 'react';
+import { Box } from 'theme-ui';
 import { ProductAttr } from '../Product';
 
-const ProductDescription: React.FC<{ product: ProductAttr }> = () => (
-  <div>Here comes product description</div>
+const ProductDescription: React.FC<{ product: ProductAttr }> = ({
+  product,
+}) => (
+  <Box sx={{ maxWidth: 820, '> div': { mb: 3 } }}>
+    <Field
+      label="Kort beskrivelse"
+      name="shortDescription"
+      value={product.shortDescription}
+    />
+    <Field
+      label="Produktbeskrivelse"
+      name="description"
+      value={product.description}
+    />
+    <Field
+      label="Teknisk beskrivelse"
+      name="technicalDescription"
+      value={product.technicalDescription}
+    />
+  </Box>
 );
 
 export default ProductDescription;

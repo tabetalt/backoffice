b6298469ff556beee322c9185fa5ab8fc0891cdf
Simen A. W. Olsen <cobraz@cobraz.no>
2020-09-21 21:09:21 +0200

    feat: Add Page component
    fix #11

--- src/modules/page/Page.tsx
+++ src/modules/page/Page.tsx
@@ -1,6 +1,26 @@
+import { Field } from '@tabetalt/kit';
 import React from 'react';
+import { Box, Heading, Select, Text } from 'theme-ui';
 import Layout from '../../components/Layout';
 
-const Page = () => <Layout>Page</Layout>;
+const Page = () => (
+  <Layout>
+    <Box sx={{ m: 5, maxWidth: 820 }}>
+      <Heading>Legg til ny innholdsside</Heading>
+      <Text sx={{ mb: 4 }}>
+        Her kan du administrere, sortere, legge til eller slette rabatter og
+        rabattkoder som kundene dine kan bruke.
+      </Text>
+      <Box sx={{ '>div': { mb: 3 } }}>
+        <Field label="Navn på siden" placeholder="Sidenavn" />
+        <Field label="URL" name="slug" placeholder="sidenavn" />
+        <Field label="Sidebeskrivelse" placeholder="Sidebeskrivelse" />
+        <Field as={Select} label="Status">
+          <option>Inaktiv</option>
+        </Field>
+      </Box>
+    </Box>
+  </Layout>
+);
 
 export default Page;

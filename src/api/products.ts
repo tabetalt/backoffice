324284bc97677cac5373804b0b9efb7b6b77fffd
Dmitry Kvokov <kvokovdl@gmail.com>
2020-11-06 14:08:44 +0200

    feat(Products): Added products linsting action buttons

--- src/api/products.ts
+++ src/api/products.ts
@@ -11,3 +11,9 @@ export const QUERY_GET_PRODUCTS = gql`
   }
   ${FRAGMENT_PRODUCT_SHORT}
 `;
+
+export const MUTATION_DELETE_PRODUCT = gql`
+  mutation DeleteProduct($id: ID!) {
+    deleteProduct(id: $id)
+  }
+`;
--- src/modules/catalog/products/ProductsListing.tsx
+++ src/modules/catalog/products/ProductsListing.tsx
@@ -1,59 +1,89 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 import { Box, IconButton } from 'theme-ui';
 import { Table, StatusLabel, icons } from '@tabetalt/kit';
 import type { CellProps } from 'react-table';
 import { Product } from '../../../api/types/Product';
+import { MUTATION_DELETE_PRODUCT, QUERY_GET_PRODUCTS } from '../../../api';
 
-const columns = [
-  {
-    Header: 'Produkter',
-    accessor: 'title',
-  },
-  {
-    Header: 'Produktpris',
-    accessor: 'price',
-  },
-  {
-    Header: 'Lagerstatus',
-    accessor: 'inventoryStatus',
-  },
-  {
-    Header: 'Statistikk',
-    accessor: 'stats',
-  },
-  {
-    Header: 'Status',
-    accessor: 'status',
-    Cell: ({ row: { original: product } }: CellProps<Product>) => {
-      const label = product.status === 'ACTIVE' ? 'Aktiv' : 'Inaktiv';
-      return <StatusLabel active={product.status === 'ACTIVE'} label={label} />;
-    },
-  },
-  {
-    Header: '',
-    accessor: 'actions',
-    Cell: () => (
-      <Box sx={{ textAlign: 'right' }}>
-        <IconButton>
-          <icons.OpenIcon />
-        </IconButton>
-        <IconButton>
-          <icons.TrashIcon />
-        </IconButton>
-        <IconButton>
-          <icons.PencilIcon />
-        </IconButton>
-      </Box>
-    ),
-  },
-];
+const { OpenIcon, TrashIcon, PencilIcon } = icons;
+
+const ProductsListingActions = ({ product }: { product: Product }) => {
+  const { id: productId } = product;
+
+  const navigate = useNavigate();
+  const [deleteProduct, { loading }] = useMutation(MUTATION_DELETE_PRODUCT, {
+    refetchQueries: [{ query: QUERY_GET_PRODUCTS }],
+  });
+
+  const onRemove = useCallback(() => {
+    if (!window.confirm('Are you sure?')) return;
+    deleteProduct({ variables: { id: productId } });
+  }, [productId, deleteProduct]);
+
+  const onOpen = () => navigate(`/catalog/product/${productId}`);
+
+  return (
+    <Box sx={{ textAlign: 'right' }}>
+      <IconButton aria-label="Open" onClick={() => onOpen} disabled={loading}>
+        <OpenIcon />
+      </IconButton>
+      <IconButton aria-label="Remove" onClick={onRemove} disabled={loading}>
+        <TrashIcon />
+      </IconButton>
+      <IconButton aria-label="Edit" onClick={onOpen} disabled={loading}>
+        <PencilIcon />
+      </IconButton>
+    </Box>
+  );
+};
 
 export interface ProductsListingProps {
   data: any[];
 }
 
-const ProductsListing: React.FC<ProductsListingProps> = ({ data }) => (
-  <Table options={{ columns, data }} />
-);
+const ProductsListing: React.FC<ProductsListingProps> = ({ data }) => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Produkter',
+        accessor: 'title',
+      },
+      {
+        Header: 'Produktpris',
+        accessor: 'price',
+      },
+      {
+        Header: 'Lagerstatus',
+        accessor: 'inventoryStatus',
+      },
+      {
+        Header: 'Statistikk',
+        accessor: 'stats',
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+        Cell: ({ row: { original: product } }: CellProps<Product>) => {
+          const label = product.status === 'ACTIVE' ? 'Aktiv' : 'Inaktiv';
+          return (
+            <StatusLabel active={product.status === 'ACTIVE'} label={label} />
+          );
+        },
+      },
+      {
+        Header: '',
+        accessor: 'actions',
+        Cell: ({ row: { original: product } }: CellProps<Product>) => (
+          <ProductsListingActions product={product} />
+        ),
+      },
+    ],
+    []
+  );
+
+  return <Table options={{ columns, data }} />;
+};
 
 export default ProductsListing;

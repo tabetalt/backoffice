d2cd994bdfc496029f7ec1d763416eb6b5a5b011
Roman Tkachev <keitarofreeman@gmail.com>
2021-04-30 16:21:45 +0300

    fix: fix logo position

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -4,9 +4,15 @@ import { Logo } from '@tabetalt/kit';
 import LoginButtons from './LoginButtons';
 
 const LoginPage: React.FC = () => (
-  <Flex sx={{ width: '100%', justifyContent: 'center' }}>
+  <Flex
+    sx={{ width: '100%', justifyContent: 'center' }}
+    style={{ marginTop: '100px' }}
+  >
     <Box>
-      <Logo size={191} />
+      <Box style={{ width: '191px', margin: '0 auto' }}>
+        <Logo />
+      </Box>
+
       <p>Logg inn ved å klikke på knappene nedenfor.</p>
       {/* <Button>Google</Button>
 

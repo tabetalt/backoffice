57e6fac2769ff4d4a0b1026e7d427295124eb976
RomanTkachev <rtkachev@klta.net>
2021-05-03 20:45:27 +0700

    fix: Wrap onAuthStateChanged observer with useEffect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from 'theme-ui';
-import { theme, Skeleton } from '@tabetalt/kit';
+import { theme, Skeleton, LoaderIcon } from '@tabetalt/kit';
 import { getRoutes } from './routing';
 import gqlClient from './api/client';
 import { auth } from './config/authConfig';
@@ -11,20 +11,33 @@ const renderLoader = () => <Skeleton />;
 
 export const App: React.FC = () => {
   const [isLoggedIn, setLoggedIn] = React.useState(false);
-  auth.onAuthStateChanged(function (user) {
-    if (user) {
-      setLoggedIn(true);
-    } else {
-      setLoggedIn(false);
-    }
-  });
+  const [isInited, setInitApp] = React.useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(function (user) {
+      setInitApp(true);
+      if (user) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    });
+
+    return function cleanup() {
+      unsubscribe();
+    };
+  }, []);
 
   const routes = useRoutes(getRoutes(isLoggedIn));
 
   return (
     <ApolloProvider client={gqlClient}>
       <ThemeProvider theme={theme}>
-        <Suspense fallback={renderLoader()}>{routes}</Suspense>
+        {!isInited ? (
+          <LoaderIcon sx={{ width: 5, display: 'block', m: '10% auto' }} />
+        ) : (
+          <Suspense fallback={renderLoader()}>{routes}</Suspense>
+        )}
       </ThemeProvider>
     </ApolloProvider>
   );
